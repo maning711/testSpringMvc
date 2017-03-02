a90ef80eae299dfa66b6cf0ccc4b9ea3f068e310
maning711 <277943578@qq.com>
2017-03-02 20:50:38 +0800

    Initial commit

---
+++ src/main/java/com/maning/ssm/controller/HomeController.java
@@ -0,0 +1,27 @@
+package com.maning.ssm.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author maning
+ * @create
+ */
+// 注解标注此类为springmvc的controller，url映射为"/home"
+@Controller
+@RequestMapping("/")
+public class HomeController {
+    //添加一个日志器
+    private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+    //映射一个action
+    @RequestMapping("/index")
+    public  String index(){
+        //输出日志文件
+        logger.info("the first jsp pages");
+        //返回一个index.jsp这个视图
+        return "index";
+    }
+}
